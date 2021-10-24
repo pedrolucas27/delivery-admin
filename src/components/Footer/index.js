bc3561927cfbd76b9d5f8770bb45c7a4f212c7e4
pedrolucas27 <plfigueiredo.27@gmail.com>
2021-10-24 01:01:48 -0300

    fix(detalhes): Correção de alguns bugs

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import 'antd/dist/antd.css';
 const { Footer } = Layout;
 function FooterSite() {
 	return (
-		<Footer style={{ textAlign: 'center', bottom: 0 }}> Master Pizza @2021 </Footer>
+		<Footer style={{ textAlign: 'center', bottom: 0 }}>master-pizza@2021</Footer>
 	);
 }
 export default FooterSite;
